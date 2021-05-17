df8ee2596c2ec3bc338f2c343481ffba1a0e781f
DuncantheeDuncan <mthembup69@gmail.com>
2021-05-17 11:22:51 +0200

    pushing

---
+++ src/main/java/com/example/springWebApp/SpringWeb5AppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeb5AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWeb5AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springWebApp/SpringWeb5AppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.springWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWeb5AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Connected!!");
+	}
+
+}
